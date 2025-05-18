ff064fb92e6ac005de7d70a9a9e1b1263dee233b
pablosalvador10 <pablosalvadorlopez11@gmail.com>
2025-05-18 16:03:19 -0500

    add app structure

--- usecases/browser_RTMedAgent/backend/server.py
+++ usecases/browser_RTMedAgent/backend/server.py
@@ -925,5 +925,4 @@ async def read_health() -> Dict[str, str]:
 # --------------------------------------------------------------------------- #
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="0.0.0.0", port=8010)
